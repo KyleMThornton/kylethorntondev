09f85125c7de0b5047bb599517f0fa64978ad8cd
KyleMThornton <kylemthornton@gmail.com>
2024-02-04 12:28:47 -0800

    Navbar fixes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,13 @@
 export default function Navbar() {
   return (
-    <nav className="w-vw bg-blue-200 h-20 flex items-center">
+    <nav className="fixed w-full bg-blue-200 h-20 flex items-center z-50 top-0">
       <div className="container flex justify-between align-middle">
         <h2>&lt;KT /&gt;</h2>
         <ul className="flex">
           <li className="px-2">Home</li>
           <li className="px-2">About</li>
+          <li className="px-2">Projects</li>
+          <li className="px-2">Resume</li>
           <li className="px-2">Contact</li>
         </ul>
       </div>
