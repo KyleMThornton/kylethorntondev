d4737173914c3b057a6fc4340e19f9ebc5ce8211
KyleMThornton <kylemthornton@gmail.com>
2024-02-04 15:35:07 -0800

    Add scroll to the top component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import ScrollToTheTop from "./components/ScrollToTheTop";
 
 export default function Home() {
   return (
     <main className="">
+      <ScrollToTheTop />
       <Hero />
       <About />
       <Projects />
